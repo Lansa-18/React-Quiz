03cf85cd19fe1783d3de515827176cd986f1983b
Mapelujo <olamideiyanda18@gmail.com>
2024-08-04 12:50:04 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -5,38 +5,38 @@ import { useEffect, useReducer } from "react";
 const initialState = {
   questions: [],
 
-  // 'loading', 'error', 'ready', 'active', 'finished' 
-  status: 'loading',
-}
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
 
 function reducer(state, action) {
-  switch(action.type) {
-    case 'dataReceived':
-      return {
-        ...state, 
-        questions: action.payload,
-        status: 'ready'
-      }
-
-    case 'dataFailed':
+  switch (action.type) {
+    case "dataReceived":
       return {
         ...state,
-        status: 'error'
-      }
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
 
     default:
-      throw new Error('Action is Unknoen')
-  } 
+      throw new Error("Action is Unknoen");
+  }
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => dispatch({type: 'dataReceived', payload: data}))
-      .catch((err) => console.error(err));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
